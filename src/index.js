6e9e80dbaf99a05a40bae06173fe4a565fe5752b
Pano Papadatos <pano.papadatos@publicrelay.com>
2018-02-18 08:09:31 -0500

    Replacing BrowserRouter with HashRouter to make things simpler on the server-side setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes/Routes';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './scss/index.scss';
 //TODO use ExtractTextPlugin to make sure css loads first
