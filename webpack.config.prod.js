2bafabd6e6908060a3e845f057058d6d5226358a
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-04-01 11:45:26 -0400

    Adding font-awesome support

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -31,6 +31,16 @@ let config = {
                     }
                 ]
             })
+        }, {
+            test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/'    // where the fonts will go
+                    // publicPath: '../'       // override the default path
+                }
+            }]
         }]
     }
 };
