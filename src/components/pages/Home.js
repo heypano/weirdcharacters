4f0ff1fe820fd2f547bbedc14fab57703fd443b7
Pano Papadatos <pano.papadatos@publicrelay.com>
2019-04-22 12:04:13 -0400

    Adding redux example (work in progress)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 import {Link} from "react-router-dom";
+import Footer from '../Footer';
+import AddTodo from '../../containers/AddTodo';
+import VisibleTodoList from '../../containers/VisibleTodoList';
+
 
 class Home extends React.Component {
     constructor(props) {
@@ -45,15 +49,15 @@ class Home extends React.Component {
                     <Link to="c1" >C1</Link><br />
                     <Link to="c2" >C2</Link><br />
                     <i className="fab fa-firefox"></i>
-                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra
-                        attention to featured content or information.</p>
-                    <hr className="my-2"/>
-                    <p>It uses utility classes for typgraphy and spacing to space content out within the larger
-                        container.</p>
                     <p className="lead">
                         <Button color="primary">Learn More</Button>
                     </p>
                 </Jumbotron>
+                <div>
+                    <AddTodo />
+                    <VisibleTodoList />
+                    <Footer />
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import Routes from './routes/Routes';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
 import { BrowserRouter as Router} from 'react-router-dom';
 import './scss/index.scss';
 
-ReactDOM.render((
-    <Router>
+const store = createStore(rootReducer)
+
+console.log(store.getState());
+
+render((
+    <Provider store={store}>
+        <Router>
         {Routes}
-    </Router>
+        </Router>
+    </Provider>
 ), document.getElementById('app'));
